79f047ef4524a9fcf54341aee96e5cc8173d3079
Felipe Farias <felipespinolafarias1@gmail.com>
2025-03-26 23:37:33 -0300

    integracao entre os componentes

--- src/components/DateDisplay.jsx
+++ src/components/DateDisplay.jsx
@@ -2,27 +2,18 @@ function DateDisplay({ years, months, days }) {
   return (
     <div className="mt-8">
       <div className="flex items-center">
-        {years === null && (
-          <span className="text-purple-600 text-6xl mr-3">{"--"}</span>
-        )}
         <span className="text-purple-600 text-6xl mr-3">
           {years !== null ? years : "--"}
         </span>
         <span className="text-black text-6xl font-bold">years</span>
       </div>
       <div className="flex items-center">
-        {months === null && (
-          <span className="text-purple-600 text-6xl mr-3">{"--"}</span>
-        )}
         <span className="text-purple-600 text-6xl mr-3">
           {months !== null ? months : "--"}
         </span>
         <span className="text-black text-6xl font-bold">months</span>
       </div>
       <div className="flex items-center">
-        {days === null && (
-          <span className="text-purple-600 text-6xl mr-3">{"--"}</span>
-        )}
         <span className="text-purple-600 text-6xl mr-3">
           {days !== null ? days : "--"}
         </span>
